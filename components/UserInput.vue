b27c588721714a063262eba15626ec9c633b2308
Elihu Cruz <elihuacruz@gmail.com>
2020-06-19 00:50:38 -0500

    init project

---
+++ components/UserInput.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="field">
+    <label for="inputText" class="label is-size-4">{{ label }}</label>
+    <div class="control">
+      <input
+        v-model="inputText"
+        @keydown.enter="storeInputName"
+        class="input"
+        type="text"
+        placeholder="A name bro"
+      />
+    </div>
+    <p>Raw debug: {{ participants }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+type UserInputState = {
+  inputText: string
+  participants: Array<string>
+}
+
+export default Vue.extend({
+  props: {
+    label: {
+      type: String,
+      default: `Hello yo 👋, Write the participant's names:`,
+    },
+  },
+  data(): UserInputState {
+    return {
+      inputText: '',
+      participants: [],
+    }
+  },
+  methods: {
+    storeInputName(): void {
+      this.participants.push(this.inputText)
+      this.inputText = ''
+      this.$emit('handleList', this.participants)
+    },
+  },
+})
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="section">
+    <div class="container">
+      <p>logo here...</p>
+      <h1 class="title">Coffe time</h1>
+      <h2 class="subtitle">Who will make the coffee today?</h2>
+      <hr />
+      <user-input @handleList="handleList" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import UserInput from '~/components/UserInput.vue'
+
+type IndexState = {
+  list: Array<string>
+}
+
+export default Vue.extend({
+  components: {
+    UserInput,
+  },
+  methods: {
+    handleList(list: Array<string>): void {
+      console.log(list)
+    },
+  },
+})
+</script>
+
+<style></style>
